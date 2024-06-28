69c0db50b0c83a985612a662b081caebcdaedbbd
Benjamin <yu.shuming0829@gmail.com>
2024-06-28 09:56:55 +0800

    config dist

---
+++ dist/spa/assets/register.7f1a9822.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index.8c1d4cf4.js";import{P as t}from"./index.esm.06a03107.js";var s=o(({app:r})=>{r.use(t)});export{s as default};
---
+++ dist/spa/assets/use-quasar.a4ac45cd.js
@@ -0,0 +1,1 @@
+import{i as a,as as r}from"./index.8c1d4cf4.js";function u(){return a(r)}export{u};
